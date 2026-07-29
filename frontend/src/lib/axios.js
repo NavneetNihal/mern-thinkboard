4cdc0093aedb38aa1b7cb74ad9d1e53e552ae269
Navneet Nihal <lakranihal0070@gmail.com>
2026-07-29 12:33:28 +0530

    prepared for the deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -10,8 +10,10 @@ const getUserId = () => {
   return userId;
 };
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
+
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: BASE_URL,
 });
 
 // Axios Request Interceptor: Automatically attach the x-user-id header to all outgoing requests
